a62280f8126a6c391488b911b9f1581221ac9528
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-02-29 12:09:16 -0300

    Revert "Revert "First Commit""
    This reverts commit c27ddbcdfe9ec6d47301ee630e5b0e6da667085f.

--- index.js
+++ index.js
@@ -3,61 +3,74 @@ const server = express()
 
 server.use(express.json())
 
-const users = ['Diego', 'Robson', 'Victor']
+const projects = []
 
-server.use((req, res, next) => {
-    console.time('Request')
-    console.log(`Método: ${req.method}; URL: ${req.url}`)
+function logRequests(req, res, next) {
 
-    next()
-    console.timeEnd('Request')
-})
+    console.count("Número de requisições");
+  
+    return next();
+}
 
-function checkUserExists(req, res, next) {
-    if (!req.body.name) {
-        return res.status(400).json({error: 'User name is requires'})
+function checkProjectInArray(req, res, next) {
+    const {id} = req.params
+    const project = projects.find(p=> p.id == id)
+    
+    if (!project) {
+        return res.status(400).json({error: 'Project does not exists'})        
     }
-
+    
     return next()
 }
 
-function checkUserInArray(req, res, next) {
-    const user = users[req.params.index]
-    if (!user) {
-        return res.status(400).json({error: 'User does not exists'})        
-    }
-    req.user = user
-    return next()
-}
+server.use(logRequests);
 
-server.get('/users', (req, res) => {
-    return res.json(users)
+server.get('/projects', (req, res) => {
+    return res.json(projects)
 })
 
-server.get('/users/:index',checkUserInArray, (req, res) => {
-    return res.json(req.user)    
-})
-
-server.post('/users',checkUserExists, (req, res) => {
-    const { name } = req.body
-    users.push(name)
+server.post('/projects', (req, res) => {
+    const {id, title } = req.body
     
-    return res.json(users)
+    const project = {
+        id,
+        title,
+        tasks: []
+      }
+
+    projects.push(project)
+    
+    return res.json(project)
 })
 
-server.put('/users/:index',checkUserInArray,checkUserExists, (req, res) => {
-    const { index } = req.params
-    const { name } = req.body
+server.post('/projects/:id/tasks',checkProjectInArray, (req, res) => {
+    const { id } = req.params    
+    const { title } = req.body
     
-    users[index] = name
-    return res.json(users)
+    const project = projects.find(p=> p.id == id)
+    
+    project.tasks.push(title)
+    
+    return res.json(project)
 })
 
-server.delete('/users/:index',checkUserInArray, (req, res) =>{
-    const { index } = req.params
-    users.splice(index, 1)
-    return res.json(users)
+server.put('/projects/:id',checkProjectInArray, (req, res) => {
+    const { id } = req.params
+    const { title } = req.body
+
+    const project = projects.find(p=> p.id == id)
+    project.title = title
     
+    return res.json(project)
+})
+
+server.delete('/projects/:id',checkProjectInArray, (req, res) =>{
+    const { id } = req.params
+    
+    const projectId = projects.findIndex(p=> p.id == id)
+    projects.splice(projectId, 1)
+
+    return res.send()    
 })
 
 server.listen(3000)
